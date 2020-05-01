a88b89650b8f86b04489b9a5195b7108b720c149
IRodrigoAlvarez <63817430+IRodrigoAlvarez@users.noreply.github.com>
2020-05-01 19:11:38 -0400

    Update main.js

--- templates/main.js
+++ templates/main.js
@@ -1,21 +1,32 @@
 var filas, dato, i, j, col, tabla, matriz;
-    function crear()
+var aux;
+let inicio;
+
+function crear()
+{
+  vertices = document.getElementById("cant_vertices").value;
+  matriz=new Array(vertices);
+  filas=vertices;
+  col=filas;
+  for(i=0;i<filas;i++)
+  {
+    matriz[i]= new Array(filas);
+    for(j=0;j<filas;j++)
     {
-      vertices = document.getElementById("cant_vertices").value;
-      matriz = new Array(vertices);
-      filas=vertices;
-      col=filas;
-
-      for(i=0;i<filas;i++)
+      aux=prompt("Es adyavente el vertice["+i+"] con "+"["+j+"]   (1:sí  0:no  /por defecto=0):");
+      if (aux<=1)
       {
-        matriz[i]= new Array(filas);
-        for(j=0;j<filas;j++){
-          matriz[i][j]=prompt("DATO:");
-
-        }                                    
+       matriz[i][j]=aux;
       }
-
+     else{
+       alert("ingrese un 1 o 0");
+       matriz[i][j]=0;
      }
+
+  
+    }
+  }
+}
 
 
 function imprimirMatriz()
@@ -27,3 +38,40 @@ function imprimirMatriz()
         
       }
     }         
+
+
+
+
+
+
+function conexo()
+{
+    let inicio=0;
+    const final=vertices;
+    let aux=0;
+    while (inicio <=final-1 )
+    {
+      for(i=0;i<final;i++)
+      {
+        for(j=0;i<final;i++)
+        {
+          if(i!=j)
+          {
+            if(matriz[i][j]!=1)
+            {
+              aux=1;
+            }
+          }
+        }
+      }
+      inicio+=1;
+    }
+    if(aux==0)
+      console.log("es conexo");
+    else
+      console.log("no es conexo");
+
+}
+
+
+
